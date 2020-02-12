781745aba8ed2682547101c8ae2ce96aff7cb59b
Samuel Tijani <omalitijani@gmail.com>
2020-02-12 15:12:06 +0100

    Add signup route

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,17 @@
 const functions = require("firebase-functions");
+const express = require('express')();
+
 const admin = require("firebase-admin");
-
 admin.initializeApp();
+const db = admin.firestore();
 
-const express = require('express');
-const app = express();
+const config = {}
 
+const firebase = require('firebase');
+firebase.initializeApp(config);
 
 app.get('/screams', (req, res) => {
-  admin
-    .firestore()
+  db
     .collection("screams")
     .orderBy('createdAt', 'desc')
     .get()
@@ -36,8 +38,7 @@ app.post('/scream', (req, res) => {
     createdAt: new Date().toISOString()
   };
 
-  admin
-    .firestore()
+  db
     .collection("screams")
     .add(newScream)
     .then(doc => {
@@ -49,5 +50,51 @@ app.post('/scream', (req, res) => {
     });
 });
 
+// sign up route
+app.post('/signup', (req, res) => {
+ 
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };    
+
+  // TODO validate data
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`).get().then(doc => {
+    if (doc.exists) {
+      return res.status(400).json({ handle: 'this handle is already taken'});
+    } else {
+      return firebase
+      .auth()
+      .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    }
+  }).then(data => {
+    userId = data.user.uid;
+    return res.data.user.getIdToken();
+  }).then((idToken) => {
+    token = idToken;
+    const userCredentials = {
+      userId,
+      email: newUser.email,
+      createdAt: new Date().toISOString(),
+      handle: req.body.handle
+    };    
+    return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+  }).then(() => {
+    return res.status.(201).json({ token });
+  })
+  .catch(err => {
+    console.error(err);
+    if (err.code === 'auth/email-already-in-use') {
+      return res.status(400).json({ email: 'Email is already in use' });
+    } else {
+      return res.status(500).json({ error: error.code });
+    }
+       
+  });
+
+  
 // https://baseurls.com/api/
-exports.api = functions.region('europe-west1').https.onRequest(app);
+exports.api = functions.region('europe-west1').https.onRequest(app); 
